b83a16053b43b649280f5eba29cc32756b3f9d76
Yantao <yyt030@gmail.com>
2016-01-28 23:09:45 +0800

    首页

--- webapp/__init__.py
+++ webapp/__init__.py
@@ -3,10 +3,12 @@
 from flask import Flask
 from flask.ext.moment import Moment
 from flask.ext.sqlalchemy import SQLAlchemy
+from flask.ext.bootstrap import Bootstrap
 from config import config
 
 moment = Moment()
 db = SQLAlchemy()
+bootstrap = Bootstrap()
 
 
 def create_app(config_name):
@@ -15,6 +17,7 @@ def create_app(config_name):
     config[config_name].init_app(app)
 
     moment.init_app(app)
+    bootstrap.init_app(app)
     db.init_app(app)
 
     if not app.debug and not app.testing and not app.config['SSL_DISABLE']:

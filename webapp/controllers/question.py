4d37a602e4ef2c44f9d4f6ad7dac3a67581911c7
Yantao <yyt030@gmail.com>
2016-02-06 15:54:05 +0800

    a

--- webapp/controllers/question.py
+++ webapp/controllers/question.py
@@ -2,7 +2,8 @@
 # coding: utf8
 __author__ = 'yueyt'
 
-from flask import Blueprint, render_template, redirect, url_for
+from flask import Blueprint, render_template, redirect, url_for, request, current_app
+from ..forms.user import LoginForm, RegisterForm
 from ..models import User, Question, Answer, Tag
 
 bp = Blueprint('question', __name__)
@@ -10,8 +11,56 @@ bp = Blueprint('question', __name__)
 
 @bp.route('/', methods=['GET', 'POST'])
 def index():
-    users = User.query.all()
-    questions = Question.query.all()
-    answers = Answer.query.all()
-    tags = Tag.query.all()
-    return redirect(url_for('site.index'))
+    login_form = LoginForm()
+    register_form = RegisterForm()
+    query = Question.query
+
+    page = request.args.get('page', 1, type=int)
+
+    pagination = query.order_by(Question.create_time.desc()).paginate(
+            page, per_page=current_app.config['FLASKY_POSTS_PER_PAGE'],
+            error_out=False)
+    questions = pagination.items
+
+    return render_template('layout.html',
+                           login_form=login_form,
+                           register_form=register_form,
+                           questions=questions, pagination=pagination)
+
+
+@bp.route('/hottest')
+def hottest():
+    login_form = LoginForm()
+    register_form = RegisterForm()
+    query = Question.query
+
+    page = request.args.get('page', 1, type=int)
+
+    pagination = query.order_by(Question.create_time.desc()).paginate(
+            page, per_page=current_app.config['FLASKY_POSTS_PER_PAGE'],
+            error_out=False)
+    questions = pagination.items
+
+    return render_template('layout.html',
+                           login_form=login_form,
+                           register_form=register_form,
+                           questions=questions, pagination=pagination, act='hottest')
+
+
+@bp.route('/unanswered')
+def unanswered():
+    login_form = LoginForm()
+    register_form = RegisterForm()
+    query = Question.query
+
+    page = request.args.get('page', 1, type=int)
+
+    pagination = query.order_by(Question.create_time.desc()).paginate(
+            page, per_page=current_app.config['FLASKY_POSTS_PER_PAGE'],
+            error_out=False)
+    questions = pagination.items
+
+    return render_template('layout.html',
+                           login_form=login_form,
+                           register_form=register_form,
+                           questions=questions, pagination=pagination, act='unanswered')

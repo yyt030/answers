76eccfbb655561b3495936d52506d75f28694d3b
Yantao <yyt030@gmail.com>
2016-03-07 13:44:36 +0800

    runserver on tornado

--- manage.py
+++ manage.py
@@ -34,6 +34,7 @@ def init_data():
 
 @manager.command
 def clear_cache():
+    """clear cache"""
     cache.init_app(app)
     with app.app_context():
         cache.clear()
@@ -41,10 +42,29 @@ def clear_cache():
 
 @manager.command
 def rungevent():
+    """run http server in gevent"""
     from gevent.wsgi import WSGIServer
 
     http_server = WSGIServer(('', 5000), app)
     http_server.serve_forever()
+
+
+@manager.command
+def runtornado():
+    """run http server in Tornado"""
+    from tornado.options import define, options
+    from tornado.wsgi import WSGIContainer
+    from tornado.httpserver import HTTPServer
+    from tornado.ioloop import IOLoop
+
+    define("port", default=5000, help="run on the given port", type=int)
+    define("develop", default=True, help="develop environment", type=bool)
+
+    options.parse_command_line()
+    http_server = HTTPServer(WSGIContainer(app))
+    http_server.listen(options.port)
+    print "visit at", "http://127.0.0.1:%s" % options.port
+    IOLoop.instance().start()
 
 
 if __name__ == '__main__':

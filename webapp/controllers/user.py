6f6c718c78be936628273c37450de69962a437ca
Yantao <yyt030@gmail.com>
2016-03-08 22:43:57 +0800

    添加用户页面

--- webapp/controllers/user.py
+++ webapp/controllers/user.py
@@ -1,12 +1,16 @@
 # coding: utf8
 from flask import Blueprint, render_template
 from ..models.user import User
+from ..forms.user import LoginForm, RegisterForm
 
 bp = Blueprint('user', __name__)
 
 
 @bp.route('/<int:id>')
 def index(id=None):
+    login_form = LoginForm()
+    register_form = RegisterForm()
+
     user = User.query.get_or_404(id)
     print '>>>', user
-    return render_template('user.html')
+    return render_template('user.html', login_form=login_form, register_form=register_form)

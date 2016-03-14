89bb07696bd0d144cb7ba2641c5b15e3246aac0c
Yantao <yyt030@gmail.com>
2016-03-14 13:53:26 +0800

    用户界面查询分页

--- webapp/controllers/user.py
+++ webapp/controllers/user.py
@@ -1,24 +1,27 @@
 # coding: utf8
-from flask import Blueprint, render_template, url_for, redirect
+from flask import Blueprint, render_template, url_for, redirect, current_app, request
 from ..models.user import User
+from ..models.question import Question, Answer
+from .. import cache
 from ..forms.user import LoginForm, RegisterForm
 
 bp = Blueprint('user', __name__)
 
 
 @bp.route('/<int:id>')
-def index(id=None):
-    user = User.query.get_or_404(id)
-    return render_template('user.html', user=user, type='answers')
-
-
 @bp.route('/<int:id>/<string:type>')
-def user_answers(id, type):
+def index(id, type='questions'):
     user = User.query.get_or_404(id)
+    page = request.args.get('page', 1, type=int)
     if type == 'answers':
-        pass
+        query = Answer.query.filter(Answer.author_id == id).order_by(Answer.create_time.desc())
     elif type == 'questions':
-        pass
+        query = Question.query.filter(Question.author_id == id).order_by(Question.create_time.desc())
     else:
-        return redirect(url_for('user.index', id=id))
-    return render_template('user.html', user=user, type=type)
+        return redirect(url_for('.index', id=id))
+
+    pagination = query.paginate(page, per_page=5,
+                                error_out=False)
+    qa = pagination.items
+    return render_template('user.html', user=user, type=type,
+                           page=page, pagination=pagination, qa=qa)
